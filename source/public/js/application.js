644f3d925d8197fbcc73c9408b373456ff087af6
Brian Lin <brian.c.lin2@gmail.com>
2015-03-21 21:30:45 -0700

    added a bunch of ajax; only get works

--- source/public/js/application.js
+++ source/public/js/application.js
@@ -13,4 +13,20 @@ $(document).ready(function() {
     $('.quote').append('<p>'+context.quote+'</p>');
   })
 
+  $('.submit_question').on('click', function(event) {
+    event.preventDefault();
+    question_title = $(this).parent().find("input[name*='question_title']").val();
+    question_content = $(this).parent().find("textarea[name*='question_content']").val();
+
+    $.ajax({
+      url: "http://localhost:3000/questions",
+      type: 'POST',
+
+      data: {question: {title: question_title, content: question_content}}
+    }).done(function(response) {
+      console.log("WE DID IT");
+      console.log(response);
+    })
+  });
+
 });
